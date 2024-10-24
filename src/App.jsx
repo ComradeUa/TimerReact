1428f0b56de60039cb2b685a6e96ec321b1be074
ComradeUa <az63.zov@gmail.com>
2024-10-24 12:47:25 +0300

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,29 @@
 import './App.css';
 import Timer from './componets/Timer.jsx';
-import { useState } from "react";
+import { useState } from 'react';
 
 function App() {
-    const [hours, setHours] = useState('00');
-    const [minutes, setMinutes] = useState('00');
-    const [seconds, setSeconds] = useState('00');
-    const [isBlackTheme, setIsBlackTheme] = useState(false);
+  const [hours, setHours] = useState('00');
+  const [minutes, setMinutes] = useState('00');
+  const [seconds, setSeconds] = useState('00');
+  const [isBlackTheme, setIsBlackTheme] = useState(false);
+  const toggleTheme = () => {
+    setIsBlackTheme((prevTheme) => !prevTheme);
+  };
 
-    const toggleTheme = () => {
-        setIsBlackTheme(prevTheme => !prevTheme);
-    };
-
-    return (
-        <div>
-            <Timer
-                hours={hours}
-                minutes={minutes}
-                seconds={seconds}
-                setHours={setHours}
-                setMinutes={setMinutes}
-                setSeconds={setSeconds}
-                isBlackTheme={isBlackTheme}
-            />
-        </div>
-    );
+  return (
+    <div>
+      <Timer
+        hours={hours}
+        minutes={minutes}
+        seconds={seconds}
+        setHours={setHours}
+        setMinutes={setMinutes}
+        setSeconds={setSeconds}
+        isBlackTheme={isBlackTheme}
+      />
+    </div>
+  );
 }
 
 export default App;
